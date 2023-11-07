cc8c5f5641a8fba48f7e23fe242fd8c475bc8f56
ulisses953 <ulisseskranzdamotta@gmail.com>
2023-11-06 21:42:22 -0300

    Desenvolvimento do model produto #1

---
+++ src/model/Produto.java
@@ -0,0 +1,69 @@
+package model;
+
+import java.util.UUID;
+
+public class Produto {
+    private UUID id;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private int quantidade;
+    private int quantidadeMinima;
+    
+    //#region get and set 
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    public int getQuantidadeMinima() {
+        return quantidadeMinima;
+    }
+    public void setQuantidadeMinima(int quantidadeMinima) {
+        this.quantidadeMinima = quantidadeMinima;
+    }
+    //#endregion
+    //#region constructor
+
+    public Produto(String nome, String descricao, double preco, int quantidade, int quantidadeMinima) {
+        this.id = UUID.randomUUID();
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        this.quantidadeMinima = quantidadeMinima;
+    }
+
+    public Produto(){
+        this.id = UUID.randomUUID();
+    }
+
+    //#endregion
+    
+
+}
